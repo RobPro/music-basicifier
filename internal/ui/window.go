1a2896d458547ec1551623d5107d9122642b2b88
Robert <robert412024@gmail.com>
2026-08-08 21:11:38 +0500

    feat(ui): add YouTube URL input field

--- internal/ui/window.go
+++ internal/ui/window.go
@@ -5,12 +5,23 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 const mainWindowTitle = "Music Basicifier"
 
 func buildMainWindow(a fyne.App) fyne.Window {
 	w := a.NewWindow(mainWindowTitle)
-	w.SetContent(container.NewWithoutLayout())
+	urlEntry := widget.NewEntry()
+	urlEntry.SetPlaceHolder("https://www.youtube.com/watch?v=siCmqvfw_1g")
+
+	content := container.NewVBox(
+		widget.NewLabel("Input"),
+		widget.NewForm(
+			widget.NewFormItem("YouTube URL", urlEntry),
+		),
+	)
+
+	w.SetContent(content)
 	return w
 }
